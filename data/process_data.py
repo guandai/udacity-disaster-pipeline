4049ef6422ab10206ae67097dda28f80f3e821bb
guandai <guandai@hotmail.com>
2021-04-30 17:59:29 +0800

    update process data

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,58 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+def load_data(messages_filepath: str, categories_filepath: str) ->  pd.DataFrame:
+    '''
+    loads message and category data
+    input:
+        messages_filepath: The file path of messages.
+        categories_filepath: The file path of categories.
+    output:
+        df: The combined dataset
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return pd.merge(messages, categories, on='id')
 
 
-def clean_data(df):
-    pass
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    input:
+        df: The combined dataset of messages and categories.
+    output:
+        df: Cleaned dataset.
+    '''
+    # get column name
+    categories = df.categories.str.split(';', expand=True)
+    col_names = categories.loc[0].apply(lambda x: x[:-2]).values.tolist()
+    categories.columns = col_names
 
+    # concat the categories back
+    cate_with_last_value = categories.applymap(lambda x: int(x[-1]))
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, cate_with_last_value], axis=1)
 
-def save_data(df, database_filename):
-    pass  
+    # drop up unused data
+    df.dropna(subset=col_names, inplace=True)
+    df.drop_duplicates(subset='message', inplace=True)
+    df.related.replace(2, 0, inplace=True)
 
+    return df
+
+def save_data(df: pd.DataFrame, database_filename: str) -> None:
+    """Export the result to a sqlite db
+    Args:
+        df: a cleaned df
+        database_filename: the file path of db
+
+    Returns:
+        None
+    """
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('messages', engine, index=False, if_exists='replace')
+    engine.dispose()
 
 def main():
     if len(sys.argv) == 4:
@@ -24,12 +65,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
